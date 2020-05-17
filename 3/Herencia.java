2169400a9d31ad45b07ebd3c913b92852d5721f3
Alexander Leonardo Lique Lamas <57840850+alexliqu09@users.noreply.github.com>
2020-05-17 18:29:20 -0500

    Add files via upload

---
+++ 3/Herencia.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.lang.Math;
+//En este ejemplo veremos como usar la herencia en Java
+//El algortimo trata de aplicar el concepto del perceptrón
+
+public class Herencia{
+
+     public static void main(String []args){
+
+     int [] intput = new int [6];
+     double bias = Math.random()/10; 
+        
+     for(int i=0;i<intput.length;i++){
+        intput[i]= (int)(Math.random()*10);
+     }
+      
+
+    Hija perceptron = new Hija(intput,bias);//Creamos un objeto de la clase Hija
+    
+    perceptron.Mostrar_Datos();//Aplicamos los métodos   
+  
+    double Y = perceptron.perceptron(intput.length);
+    
+    Y=perceptron.sigmoide(Y); 
+   
+    if(Y>=0.51){
+    System.out.println(1+"\n");
+    
+   }else{
+    System.out.print(0+"\n");
+   }  
+   
+   
+
+
+ }
+
+
+}
